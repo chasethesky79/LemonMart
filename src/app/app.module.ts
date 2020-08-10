01227aefd439bec0d8dbb40aa225bd931034bf50
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2020-08-09 19:43:15 -0500

    feature/initial-commit
    Generated modules for the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
+import { ManagerModule } from './manager/manager.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { PosModule } from './pos/pos.module';
+import { UserModule } from './user/user.module';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    ManagerModule,
+    InventoryModule,
+    PosModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagenotfound',
+  template: `
+    <p>
+      This page doesn't exist. Go back to <a routerLink="/home">Home</a>
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PagenotfoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
