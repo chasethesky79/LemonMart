6d038db33d25bfe5701b75fa42b9e7025b7a010d
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2020-08-10 21:10:50 -0500

    feature/initial-commit
    Implemented flex layout for the home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
     declarations: [AppComponent, HomeComponent, PagenotfoundComponent],
@@ -29,7 +30,8 @@ import {HttpClientModule} from '@angular/common/http';
         PosModule,
         UserModule,
         BrowserAnimationsModule,
-        HttpClientModule
+        HttpClientModule,
+        FlexLayoutModule
     ],
     providers: [],
     bootstrap: [AppComponent],
