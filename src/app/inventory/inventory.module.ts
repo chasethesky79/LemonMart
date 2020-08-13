1185776440bf0656ce4080ca3bd7c6a27c613626
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2020-08-12 21:54:11 -0500

    feature/initial-commit
    Generated components for the inventory module

--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { InventoryRoutingModule } from './inventory-routing.module';
 import { InventoryComponent } from './inventory/inventory.component';
+import { StockEntryComponent } from './stock-entry/stock-entry.component';
+import { ProductsComponent } from './products/products.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-    declarations: [InventoryComponent],
+    declarations: [InventoryComponent, StockEntryComponent, ProductsComponent, DashboardComponent],
     imports: [CommonModule, InventoryRoutingModule],
 })
 export class InventoryModule {}
