e3f53e6f6384a371e7be51a2937440804478adaa
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2020-08-15 22:28:44 -0500

    feature/initial-commit
    Added more configuration for CircleCI

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,23 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromiumNoSandbox'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222',
+        ],
+        debug: true
+      },
+      ChromiumNoSandbox: {
+        base: 'ChromiumHeadless',
+        flags: ['--no-sandbox', '--disable-translate', '--disable-extensions']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
