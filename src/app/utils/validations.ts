19421a351faab11677d77a5738c25cf12c410b54
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2020-09-10 21:29:57 -0500

    master
    Added more code changes

--- src/app/utils/validations.ts
+++ src/app/utils/validations.ts
@@ -1,4 +1,19 @@
 import { Validators } from '@angular/forms';
+import { usStates } from '../models/user-data';
 
 export const emailValidator = [Validators.required, Validators.email];
 export const passwordValidator = [Validators.required, Validators.minLength(8), Validators.maxLength(50)];
+export const optionalTextValidator = [Validators.minLength(2), Validators.maxLength(50)];
+export const requiredTextValidator = optionalTextValidator.concat(Validators.required);
+export const oneCharValidator = [Validators.minLength(1), Validators.maxLength(1)];
+export const birthDateValidator = [
+    Validators.required,
+    Validators.min(new Date().getFullYear() - 100),
+    Validators.max(new Date().getFullYear()),
+];
+export const usaZipCodeValidator = [Validators.required, Validators.pattern(/^\d{5}$|^\d{5}-\d{4}$/)];
+export const usaPhoneNumberValidator = [Validators.required, Validators.pattern(/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/)];
+export const usStateFilter = (value: string) =>
+    usStates.filter(
+        (usState) => usState?.code?.toLowerCase() === value.toLowerCase() || usState.name?.toLowerCase().includes(value.toLowerCase())
+    );
