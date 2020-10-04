ed218a719c7de6e685750304587f43229e40269c
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2020-10-03 19:47:50 -0500

    master
    Implemented the -'Add Phone' function that adds a default phone

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -148,7 +148,7 @@ export class ProfileComponent implements OnInit {
     addPhoneControl(): void {
       this.phones = [...this.phones, {
         id: this.phones.length + 1,
-        number: '233-344-2312',
+        number: '',
         type: PhoneType.Mobile
       }];
       console.log(`THIS PHONES ${JSON.stringify(this.phones)}`);
